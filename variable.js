4b04d5293d757edd4a453ead4a96f1cdceb8bfa1
Rakshith hegde <rakshithhgd@gmail.com>
2021-05-10 22:09:15 +0530

    10-05-21

---
+++ variable.js
@@ -0,0 +1,19 @@
+//no declaring variables
+
+let num=2+2
+console.log(num)
+
+console.log(num+3)
+
+num=8  //as num is already declared we should'nt use let again for the same variable declaration//
+console.log(num)
+
+let user666='RK'
+console.log(user666)
+
+let userName = "Rakshith"
+console.log(userName) //camelcase
+
+let admin="RK's PC"
+console.log(admin)
+
