e34c5e7aaaa00d2af714ba752a78ec538e7863fa
Rakshith hegde <rakshithhgd@gmail.com>
2021-05-15 19:36:43 +0530

    15-05-21

---
+++ text.js
@@ -0,0 +1,2 @@
+var x = "astonmartin,bugati,chervolet,dodge,eicher,ford,GMC,Hyndai,Isuzu,Jaguar,Koenigssegg,Lexus,Mercedes,Nissan,Ocelot,Pagani,Qoros,Rolls-royce,Shelby,Tesla,UAZ,Volvo,W-motors,xpeng,zenos";
+
