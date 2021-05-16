f53b8fabf9a5fdece6fcae8a37c675f0ed18ba14
Rakshith hegde <rakshithhgd@gmail.com>
2021-05-16 19:28:47 +0530

    16-05-21

---
+++ num.js
@@ -0,0 +1,25 @@
+var x=3.14; // number with decimals
+var y = 3; //number without decimals
+
+console.log(x);
+console.log(y);
+
+//exponent form
+var a = 123e5;
+console.log(a);
+
+var b = 123e-5;
+console.log(b);
+
+//precision
+var c = 9999999999999999;
+console.log(c);
+
+var x = 0.2 + 0.1;
+console.log( "0.2 + 0.1 = " + x);
+
+//arithmetic
+var x = (0.2 * 10 + 0.1 * 10) / 10; 
+console.log(x);
+
+
