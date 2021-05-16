7d18deb66047f3ba27cbc66ce5762f67dff13f74
Rakshith hegde <rakshithhgd@gmail.com>
2021-05-16 19:43:46 +0530

    16-05-21

--- numadd.js
+++ numadd.js
@@ -8,3 +8,28 @@ var x = "10";
 var y = "20";
 var z = x + y; 
 console.log(z);
+
+var x = "100";
+var y = "10";
+var z = x + y; 
+console.log(z);
+
+var x = "100";
+var y = "10";
+var z = x - y; 
+console.log(z);
+
+var x = "100";
+var y = "10";
+var z = x * y;   
+console.log(z);
+
+
+
+var x = "100";
+var y = "10";
+var z = x / y; 
+console.log(z);
+
+var x = 100 / "Apple";
+console.log(x);
